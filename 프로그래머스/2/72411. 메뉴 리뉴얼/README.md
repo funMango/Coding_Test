58da17809fd8d4abeb589de96d4358c416c80d40
MinhoLee <138420832+funMango@users.noreply.github.com>
2024-05-17 19:27:21 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.29 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.23 ms
+메모리: 10.4 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 17일 19:01:48
+2024년 05월 17일 19:27:19
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,27 +1,16 @@
 from itertools import combinations
+from collections import Counter
+
 import math
 
 def solution(orders, course):
     answer = []
     
-    for i in range(len(orders)):
-        orders[i] = "".join(sorted(list(orders[i])))
-    
     for c in course:
-        order_dic = {}
+        combi = []
         for order in orders:
-            combi = combinations(order, c)
-            for str in combi:
-                menus = "".join(str)
-                if menus in order_dic:
-                    order_dic[menus] += 1
-                else:
-                    order_dic[menus] = 1
-        
-        if order_dic:            
-            m = max([v for k,v in order_dic.items()])
-            if m >= 2:
-                courses = [k for k,v in order_dic.items() if v == m]
-                answer += courses
-        
+            combi += combinations(sorted(order), c)
+        most_ordered = Counter(combi).most_common()        
+        max_count = most_ordered[0][1] if most_ordered else 0
+        answer += ["".join(k) for k,v in most_ordered if v > 1 and v == max_count]
     return sorted(answer)
