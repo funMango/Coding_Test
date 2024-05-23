ee3239b7b6f939c3e0a24f6fc5005590e2bee95b
MinhoLee <138420832+funMango@users.noreply.github.com>
2024-05-23 12:23:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.41 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,4 +1,3 @@
 def solution(nums):
-    arr = set([n for n in nums])
-    m = len(nums) / 2
-    return len(arr) if len(arr) < m else m   
+    arr = set([n for n in nums])    
+    return min(len(arr), len(nums) / 2)
