a0667e1b2925bfb96f91fac4ecf54e571c62fe98
MinhoLee <138420832+funMango@users.noreply.github.com>
2024-05-23 13:55:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,26 +1,12 @@
-from collections import Counter
-
 def solution(n, words):
-    answer = []
-    stack = []
-    words_set = set()
+    prev = words[0][0]
+    used = set()
     
-    # 앞글자와 단어가 다른 경우
-    for i in range(len(words)):
-        if not stack:
-            stack.append(words[i])            
-        else:
-            if stack[-1][-1] == words[i][0]:
-                stack.pop()
-                stack.append(words[i])
-            elif stack[-1][-1] != words[i][0] or len(words[i]) <= 1:
-                print(i)
-                return [i%n+1, i//n+1]
-        
-        # 중복확인
-        if words[i] not in words_set:
-            words_set.add(words[i])
-        else:
+    for i, word in enumerate(words):
+        if prev[-1] != word[0] or word in used:
             return [i%n+1, i//n+1]
-
+        else:
+            prev = word
+            used.add(word)
+    
     return [0,0]
